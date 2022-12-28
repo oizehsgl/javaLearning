ea22cc6eb16005ea1faaac9b61dc492117d90759
oizehsgl <oizehsgl@gmail.com>
2022-12-28 15:24:16 +0800

    fix: java注释

---
+++ basic/src/main/java/org/oizehsgl/sub/Comment.java
@@ -0,0 +1,16 @@
+package org.oizehsgl.sub;
+
+/**
+ * 注释
+ *
+ * @author oizehsgl
+ * @since 2022/12/23
+ */
+public class Comment {
+    public static void main(String[] args) { // main start
+        System.out.println("注释");
+        System.out.printf("%s: %s%n","单行","//");
+        System.out.printf("%s: %s%n","多行","\n/*\n*\n*/");
+        System.out.printf("%s: %s%n","文档行","\n/**\n*\n*/");
+    }
+}
