f9607b87c1534539fd91e6e650a58f15618dfca4
oizehsgl <oizehsgl@gmail.com>
2023-01-03 10:02:50 +0800

    feat(grammar): 添加数据类型

---
+++ grammar/src/main/java/org/oizehsgl/sub/Type.java
@@ -0,0 +1,25 @@
+package org.oizehsgl.sub;
+
+/**
+ * 类型
+ *
+ * @author oizehsgl
+ * @since 2022/12/29
+ */
+public class Type {
+    public static void main(String[] args) {
+        System.out.println("基本类型");
+        System.out.println("整数");
+        System.out.printf("类型:%s\t大小:%s\n","byte","1");
+        System.out.printf("类型:%s\t大小:%s\n","short","2");
+        System.out.printf("类型:%s\t大小:%s\n","int","4");
+        System.out.printf("类型:%s\t大小:%s\n","long","8");
+        System.out.println("浮点");
+        System.out.printf("类型:%s\t大小:%s\n","float","4");
+        System.out.printf("类型:%s\t大小:%s\n","double","8");
+        System.out.println("字符");
+        System.out.printf("类型:%s\t大小:%s\n","char","2");
+        System.out.println("布尔");
+        System.out.printf("类型:%s\t大小:%s\n","boolean","1");
+    }
+}
---
+++ grammar/src/test/java/org/oizehsgl/sub/TypeTest.java
@@ -0,0 +1,32 @@
+package org.oizehsgl.sub;
+
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+
+/**
+ * @author oizehsgl
+ * @since 2022/12/29
+ */
+public class TypeTest {
+    @Test
+    public void testSize() {
+        getSize(byte.class,100000000);
+        getSize(boolean.class,100000000);
+        getSize(short.class,100000000);
+        getSize(char.class,100000000);
+        getSize(int.class,100000000);
+        getSize(float.class,100000000);
+        getSize(long.class,100000000);
+        getSize(double.class,100000000);
+    }
+    private static void  getSize(Class type,int length) {
+        Runtime runtime = Runtime.getRuntime();
+        System.gc();
+        long startMem = runtime.totalMemory() - runtime.freeMemory();
+        Object array=Array.newInstance(type,length);
+        System.gc();
+        long endMem = runtime.totalMemory() - runtime.freeMemory();
+        System.out.printf("类型%-4s\t总数%d\t总占用%s\t平均占用%s\n", type.getName(),length,endMem-startMem,(endMem-startMem)/(double)length);
+    }
+}
