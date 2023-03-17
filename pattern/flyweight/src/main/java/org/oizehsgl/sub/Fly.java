b11d9679fc4ceeae98928f3d10398644fd332fa9
oizehsgl <oizehsgl@gmail.com>
2023-03-17 11:20:58 +0800

    feat(pattern): flyweight

---
+++ pattern/flyweight/src/main/java/org/oizehsgl/sub/Fly.java
@@ -0,0 +1,11 @@
+package org.oizehsgl.sub;
+
+/**
+ * fly
+ *
+ * @author oizehsgl
+ * @since 3/17/23
+ */
+public interface Fly {
+    void eat();
+}
---
+++ pattern/flyweight/src/main/java/org/oizehsgl/sub/FlyFood.java
@@ -0,0 +1,11 @@
+package org.oizehsgl.sub;
+
+/**
+ * flyFood
+ *
+ * @author oizehsgl
+ * @since 3/17/23
+ */
+public enum FlyFood {
+    FRUIT, SHIT
+}
