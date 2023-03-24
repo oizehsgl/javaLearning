a4ce299c6ae8a12c21440bc62289b8a0a1f7c02b
oizehsgl <oizehsgl@gmail.com>
2023-03-24 13:48:14 +0800

    feta(type): enum测试

---
+++ basic/type/src/main/java/org/oizehsgl/sub/EnumDemo.java
@@ -0,0 +1,11 @@
+package org.oizehsgl.sub;
+
+/**
+ * enumDemo
+ *
+ * @author oizehsgl
+ * @since 3/24/23
+ */
+public enum EnumDemo {
+    VALUE1,VALUE2
+}
---
+++ basic/type/src/test/java/org/oizehsgl/sub/EnumTest.java
@@ -0,0 +1,17 @@
+package org.oizehsgl.sub;
+
+import org.junit.Test;
+
+/**
+ * enumTest
+ *
+ * @author oizehsgl
+ * @since 3/24/23
+ */
+public class EnumTest {
+    @Test
+    public void testEnum() {
+        System.out.println(null == EnumDemo.VALUE1);
+        System.out.println(EnumDemo.VALUE1 == EnumDemo.VALUE1);
+    }
+}
