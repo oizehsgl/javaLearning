3ea0fee868c425080f744e96769bbdf482bcd7b5
oizehsgl <oizehsgl@gmail.com>
2023-04-08 23:18:09 +0800

    feat(project): demo项目测试

---
+++ project/demo/src/test/java/org/oizehsgl/sub/AppTest.java
@@ -0,0 +1,20 @@
+package org.oizehsgl.sub;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Unit test for simple App.
+ */
+@SpringBootTest
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testApp() {
+        Assertions.assertTrue(true);
+    }
+}
