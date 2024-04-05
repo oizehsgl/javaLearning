091448fae47fdbd02964928901256622a1144fb3
yueyuanzhi <yuanzhi.yue@inrevo.io>
2024-04-05 21:18:01 +0800

    feat: 添加框架springboot模块
    1. 删除git子模块
    2. 添加readme说明

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # javaLearning
 java学习笔记
+# basic
+# algorithm
+# data-structure
+# pattern
+# library
+# framework
+# project
---
+++ javaLearning-framework-springBoot/src/main/java/org/oizehsgl/javaLearning/framework/springBoot/JavaLearningFrameworkSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.oizehsgl.javaLearning.framework.springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearningFrameworkSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLearningFrameworkSpringBootApplication.class, args);
+	}
+
+}
