f646bf78e360d7357c111caa3cbe8a668d3757ec
yueyuanzhi <yuanzhi.yue@inrevo.io>
2024-04-06 14:51:54 +0800

    feat: 添加gson示例

---
+++ javaLearning-library-gson/src/main/java/org/oizehsgl/javaLearning/library/gson/JavaLearningLibraryGsonApplication.java
@@ -0,0 +1,13 @@
+package org.oizehsgl.javaLearning.library.gson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearningLibraryGsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLearningLibraryGsonApplication.class, args);
+	}
+
+}
