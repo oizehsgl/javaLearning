b3814891614e1e91e4edcb47d83e4ce5e933154b
yueyuanzhi <yuanzhi.yue@inrevo.io>
2024-04-06 18:59:44 +0800

    feat: 添加logback示例

---
+++ javaLearning-library-logback/src/main/java/org/oizehsgl/javaLearning/library/logback/JavaLearningLibraryLogbackApplication.java
@@ -0,0 +1,13 @@
+package org.oizehsgl.javaLearning.library.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearningLibraryLogbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLearningLibraryLogbackApplication.class, args);
+	}
+
+}
---
+++ javaLearning-library-logback/src/test/java/org/oizehsgl/javaLearning/library/logback/JavaLearningLibraryLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oizehsgl.javaLearning.library.logback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearningLibraryLogbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
