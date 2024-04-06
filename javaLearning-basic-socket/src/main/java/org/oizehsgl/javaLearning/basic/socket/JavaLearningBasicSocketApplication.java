dd738eb963cb63b30c10d66185f075d7fb939f63
yueyuanzhi <yuanzhi.yue@inrevo.io>
2024-04-06 12:09:16 +0800

    feat: 添加socket示例

---
+++ javaLearning-basic-socket/src/main/java/org/oizehsgl/javaLearning/basic/socket/JavaLearningBasicSocketApplication.java
@@ -0,0 +1,13 @@
+package org.oizehsgl.javaLearning.basic.socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearningBasicSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLearningBasicSocketApplication.class, args);
+	}
+
+}
---
+++ javaLearning-basic-socket/src/test/java/org/oizehsgl/javaLearning/basic/socket/JavaLearningBasicSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oizehsgl.javaLearning.basic.socket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearningBasicSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
