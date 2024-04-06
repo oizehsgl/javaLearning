cba52aaa321a71f54bdad29467110e9ebd1f3b24
yueyuanzhi <yuanzhi.yue@inrevo.io>
2024-04-06 11:51:40 +0800

    feat: 添加protobuf

---
+++ javaLearning-library-protobuf/src/test/java/org/oizehsgl/javaLearning/library/protobuf/JavaLearningLibraryProtobufApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oizehsgl.javaLearning.library.protobuf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearningLibraryProtobufApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ javaLearning-library-protobuf/src/test/java/org/oizehsgl/javaLearning/library/protobuf/service/HelloWorldServiceProtoTest.java
@@ -0,0 +1,19 @@
+package org.oizehsgl.javaLearning.library.protobuf.service;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.ResourceLock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author yueyuanzhi
+ */
+@SpringBootTest
+class HelloWorldServiceProtoTest {
+    @Test
+    public void test(){
+        System.out.println(HelloWorldServiceProto.getDescriptor());
+    }
+}
