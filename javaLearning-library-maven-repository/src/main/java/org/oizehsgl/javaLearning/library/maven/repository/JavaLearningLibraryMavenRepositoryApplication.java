f524a43691eb91a49a92833ae3543e60a700441d
yueyuanzhi <yuanzhi.yue@inrevo.io>
2024-04-08 01:47:15 +0800

    feat: 添加maven示例

---
+++ javaLearning-library-maven-repository/src/main/java/org/oizehsgl/javaLearning/library/maven/repository/JavaLearningLibraryMavenRepositoryApplication.java
@@ -0,0 +1,13 @@
+package org.oizehsgl.javaLearning.library.maven.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearningLibraryMavenRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLearningLibraryMavenRepositoryApplication.class, args);
+	}
+
+}
--- javaLearning-library-sqlite/src/main/resources/db/demo_table.sql
+++
@@ -1,4 +0,0 @@
-create table if not exists demo_table(
-   id integer primary key autoincrement,
-   name char(255) not null
-);
