86c8ed531ea672c33ce8de996437f8b26ee20b5b
yueyuanzhi <yuanzhi.yue@inrevo.io>
2024-04-27 08:38:59 +0800

    feat: 添加scheduling示例

---
+++ javaLearning-framework-springBoot-scheduling/src/main/java/org/oizehsgl/javaLearning/framework/springBoot/scheduling/scheduling/DemoScheduling.java
@@ -0,0 +1,31 @@
+package org.oizehsgl.javaLearning.framework.springBoot.scheduling.scheduling;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * 示例调度
+ *
+ * @author yueyuanzhi
+ */
+@Slf4j
+@Component
+public class DemoScheduling {
+
+    @Scheduled(cron = "*/1 * * * * ?")
+    public void test1() {
+        System.out.println("test cron1");
+        log.warn("1");
+    }
+    @Scheduled(cron = "*/1 * * * * ?")
+    public void test2() {
+        System.out.println("test cron2");
+        log.warn("2");
+    }
+    @Scheduled(cron = "*/1 * * * * ?")
+    public void test3() {
+        System.out.println("test cron3");
+        log.warn("3");
+    }
+}
