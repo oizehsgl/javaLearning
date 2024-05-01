b29e6de6db170c8a3e3db2c655eb13f2c69a003e
yueyuanzhi <yuanzhi.yue@inrevo.io>
2024-05-01 23:36:04 +0800

    feat: 重启示例

---
+++ javaLearning-framework-springBoot-restart/src/main/java/org/oizehsgl/javaLearning/framework/springBoot/restart/service/RestartService.java
@@ -0,0 +1,20 @@
+package org.oizehsgl.javaLearning.framework.springBoot.restart.service;
+
+import jakarta.annotation.Resource;
+import org.springframework.cloud.context.restart.RestartEndpoint;
+import org.springframework.stereotype.Service;
+
+/**
+ * 重启服务层
+ *
+ * @author yueyuanzhi
+ */
+@Service
+public class RestartService {
+    @Resource
+    private RestartEndpoint restartEndpoint;
+
+    public void restartApp() {
+        restartEndpoint.restart();
+    }
+}
---
+++ javaLearning-framework-springBoot-restart/src/test/java/org/oizehsgl/javaLearning/framework/springBoot/restart/JavaLearningFrameworkSpringBootRestartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oizehsgl.javaLearning.framework.springBoot.restart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearningFrameworkSpringBootRestartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
