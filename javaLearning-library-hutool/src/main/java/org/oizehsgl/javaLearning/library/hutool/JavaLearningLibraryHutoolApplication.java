364dd24f10425198ef59b708d545ea5816f239c4
yueyuanzhi <yuanzhi.yue@inrevo.io>
2024-05-01 23:45:03 +0800

    feat: hutool示例

---
+++ javaLearning-library-hutool/src/main/java/org/oizehsgl/javaLearning/library/hutool/JavaLearningLibraryHutoolApplication.java
@@ -0,0 +1,13 @@
+package org.oizehsgl.javaLearning.library.hutool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearningLibraryHutoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLearningLibraryHutoolApplication.class, args);
+	}
+
+}
---
+++ javaLearning-library-hutool/src/test/java/org/oizehsgl/javaLearning/library/hutool/JavaLearningLibraryHutoolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oizehsgl.javaLearning.library.hutool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearningLibraryHutoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
