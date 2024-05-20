7a3cb1facd080e997c864d8726f7bdedcb3840f2
oizehsgl <oizehsgl@gmail.com>
2024-05-20 22:04:49 +0800

    feat: 测试git配置

---
+++ oizehsgl-starter-demo-wrapper-spring-boot-starter/src/main/java/org/oizehsgl/starter/demo/wrapper/config/StarterDemoWrapperAutoConfiguration.java
@@ -0,0 +1,19 @@
+package org.oizehsgl.starter.demo.wrapper.config;
+
+import org.oizehsgl.starter.demo.properties.StarterDemoProperties;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author oizehsgl
+ */
+@AutoConfiguration
+public class StarterDemoWrapperAutoConfiguration {
+    @Bean
+    public StarterDemoProperties starterDemoProperties() {
+        StarterDemoProperties starterDemoProperties = new StarterDemoProperties();
+        starterDemoProperties.setCode("2");
+        starterDemoProperties.setName("starter-demo-wrapper");
+        return starterDemoProperties;
+    }
+}
