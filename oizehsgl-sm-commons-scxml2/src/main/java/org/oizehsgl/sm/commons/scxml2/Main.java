edc7bb769c11e1788901df59d57d85e4d4893050
oizehsgl <oizehsgl@gmail.com>
2024-05-26 11:57:01 +0800

    feat: scxml补充

--- oizehsgl-sm-commons-scxml2/src/main/java/org/oizehsgl/sm/commons/scxml2/Main.java
+++ oizehsgl-sm-commons-scxml2/src/main/java/org/oizehsgl/sm/commons/scxml2/Main.java
@@ -18,56 +18,66 @@ package org.oizehsgl.sm.commons.scxml2;
 
 import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 import org.apache.commons.scxml2.Context;
 import org.apache.commons.scxml2.Evaluator;
 import org.apache.commons.scxml2.SCXMLExecutor;
+import org.apache.commons.scxml2.TriggerEvent;
 import org.apache.commons.scxml2.env.SimpleErrorReporter;
 import org.apache.commons.scxml2.env.jexl.JexlEvaluator;
 import org.apache.commons.scxml2.io.SCXMLReader;
+import org.apache.commons.scxml2.model.EnterableState;
 import org.apache.commons.scxml2.model.SCXML;
-import org.springframework.boot.web.embedded.netty.NettyWebServer;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.util.ResourceUtils;
 
 public class Main {
 
+  // SCXML model source URL
+  private static final URL SCXML;
 
-    // SCXML model source URL
-    private static final URL SCXML;
-
-    static {
-        try {
-            SCXML = ResourceUtils.getURL("classpath:stopwatch.xml");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+  static {
+    try {
+      SCXML = ResourceUtils.getURL("classpath:stopwatch.xml");
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    public static void main(String [] args) throws Exception {
-        // evaluator instance which is used by SCXML engine to evaluate expressions in SCXML
-        Evaluator evaluator = new JexlEvaluator();
-        // engine to execute the scxml instance
-        SCXMLExecutor executor = new SCXMLExecutor(evaluator, null, new SimpleErrorReporter());
+  public static void main(String[] args) throws Exception {
+    // evaluator instance which is used by SCXML engine to evaluate expressions in SCXML
+    Evaluator evaluator = new JexlEvaluator();
+    // engine to execute the scxml instance
+    SCXMLExecutor executor = new SCXMLExecutor(evaluator, null, new SimpleErrorReporter());
 
-        // parse SCXML URL into SCXML model
-        SCXML scxml = SCXMLReader.read(SCXML);
-        // set state machine (scxml instance) to execute
-        executor.setStateMachine(scxml);
+    // parse SCXML URL into SCXML model
+    SCXML scxml = SCXMLReader.read(SCXML);
+    // set state machine (scxml instance) to execute
+    executor.setStateMachine(scxml);
 
-        // create root context storing variables and being used by evaluator
-        Context rootContext = evaluator.newContext(null);
+    // create root context storing variables and being used by evaluator
+    Context rootContext = evaluator.newContext(null);
 
-        // create stopWatch object and add it to rootContext
-        // to be able to script with that in SCXML.
-        StopWatch stopWatch = new StopWatch();
-        rootContext.set("stopWatch", stopWatch);
+    // create stopWatch object and add it to rootContext
+    // to be able to script with that in SCXML.
+    StopWatch stopWatch = new StopWatch();
+    rootContext.set("stopWatch", stopWatch);
+    rootContext.set("stopWatch1", stopWatch);
 
-        // set the root context for the engine
-        executor.setRootContext(rootContext);
+    // set the root context for the engine
+    executor.setRootContext(rootContext);
 
-        // initiate the execution of the state machine
-        executor.go();
+    // initiate the execution of the state machine
+    executor.go();
 
-        new StopWatchFrame(executor);
+    System.out.println(executor.getRootContext());
+    for (EnterableState state : executor.getCurrentStatus().getStates()) {
+      System.out.println(state.getId());
     }
+    executor.triggerEvent(new TriggerEvent("watch.start", TriggerEvent.SIGNAL_EVENT));
+    System.out.println(executor.getCurrentStatus());
+
+    TimeUnit.SECONDS.sleep(10);
+
+    // new StopWatchFrame(executor);
+  }
 }
