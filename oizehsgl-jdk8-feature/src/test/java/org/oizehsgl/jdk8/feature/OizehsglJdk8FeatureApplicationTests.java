41f09d39f0639d26eb1c6a4d38cbcf28c9797207
oizehsgl <oizehsgl@gmail.com>
2024-05-27 02:53:03 +0800

    feat: completableFeature

---
+++ oizehsgl-jdk8-feature/src/test/java/org/oizehsgl/jdk8/feature/OizehsglJdk8FeatureApplicationTests.java
@@ -0,0 +1,14 @@
+package org.oizehsgl.jdk8.feature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CompletableFuture;
+
+@SpringBootTest
+class OizehsglJdk8FeatureApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
