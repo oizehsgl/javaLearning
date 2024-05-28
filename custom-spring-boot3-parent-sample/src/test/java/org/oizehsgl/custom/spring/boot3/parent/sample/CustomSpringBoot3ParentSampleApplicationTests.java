69f95863a7627b580c0e5c44f5fc1bf2438d017e
oizehsgl <oizehsgl@gmail.com>
2024-05-29 02:18:40 +0800

    feat: custom spring parent 配置

---
+++ custom-spring-boot3-parent-sample/src/test/java/org/oizehsgl/custom/spring/boot3/parent/sample/CustomSpringBoot3ParentSampleApplicationTests.java
@@ -0,0 +1,10 @@
+package org.oizehsgl.custom.spring.boot3.parent.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomSpringBoot3ParentSampleApplicationTests {
+  @Test
+  void contextLoads() {}
+}
