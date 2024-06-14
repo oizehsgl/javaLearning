f52a40534d28d26aeb082e55821218023dbf70df
oizehsgl <oizehsgl@gmail.com>
2024-06-14 09:18:35 +0800

    feat: properties实现

---
+++ oizehsgl-springBoot-properties/src/main/java/org/oizehsgl/springBoot/properties/OizehsglSpringBootPropertiesApplication.java
@@ -0,0 +1,12 @@
+package org.oizehsgl.springBoot.properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OizehsglSpringBootPropertiesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OizehsglSpringBootPropertiesApplication.class, args);
+  }
+}
